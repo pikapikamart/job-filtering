0ced8566e6acd2357925dc34ebdc8c55f21b297c
pikapikamart <80995899+pikapikamart@users.noreply.github.com>
2021-03-31 08:03:56 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,63 @@
+# Frontend Mentor - Job listings with filtering solution
+
+This is a solution to the [Job listings with filtering challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/job-listings-with-filtering-ivstIPCt). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+
+### Screenshot
+
+![Desktop-view](finished/desktop.png)
+I did not include the mobile screenshots since it is very long ahahaa
+
+### Links
+
+Live Site URL: [Live site of the challenge hosted here](https://pikapikamart.github.io/frontendmentor-job-filtering/)
+
+## My process
+
+Hello this is my twelfth challenge submission here in front end mentor and this was a fun challenge for me. I got late in my everyday fronendmentor since my study started yesterday, you know, school. But hey, I managed to do it and I really liked it. Hope you to as well^
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- FlexBox
+- Javascript
+- Sass preprocessor
+
+I said that I won't use any frameworks and build it from scratch to be more exciting and challenging
+
+### What I learned
+
+Working with JSON file are definitely the best choice in every challenge that requires dynamic implementation
+
+### Continued development
+
+I want to focus more on creating more complex designs and also designing things on my own time
+
+## Author
+
+- Website - Well I haven't made my profile portfolio, gonna make it sooon when I know a lot of stuffs
+- Frontend Mentor - [@pikamart](https://www.frontendmentor.io/profile/pikamart)
+- Twitter - [@RaymartPamplona](https://twitter.com/RaymartPamplona)
---
+++ script.js
@@ -0,0 +1,156 @@
+// To be filterd 
+let stackElements = [];
+let filteredStack = [];
+let currentStack = {};
+
+const container = document.querySelector(".container");
+const currentitems = document.querySelector(".current-stack");
+
+// Getters
+function el(e) {
+    return document.querySelector(e);
+}
+
+function els(e) {
+    return document.querySelectorAll(e);
+}
+
+function filterStack(item, element) {
+    let ar = [];
+    currentitems.appendChild(element);
+    stackElements.forEach(stack => {
+        if (stack.available && !stack.filter.includes(item)) {
+            stack.element.classList.add("d-none");
+            ar.push(stack.element);
+            stack.available = false;
+        }  
+    })
+    currentStack[item] = ar;
+    checkFilterContainer();
+}
+
+function filterEvent(element) {
+    element.addEventListener("click", (e) => {
+        e.preventDefault();
+        delete currentStack[element.textContent];
+        unFilterStack();
+        element.remove();
+        checkFilterContainer();
+    })
+}
+
+function unFilterStack() {
+    stackElements.forEach(stack =>{
+        let check = Object.keys(currentStack).every(key => stack.filter.includes(key));
+        if (check) {
+            stack.element.classList.remove("d-none");
+            stack.available = true;
+        }
+    })
+}
+
+function checkFilterContainer() {
+    if(!currentitems.childNodes.length) {
+        currentitems.classList.add("d-none")
+    } else{
+        currentitems.classList.remove("d-none")
+    }
+}
+
+
+
+// The reason for using promise is that since we are fetching something
+// from the server, a promise is proposed in here
+const promise = new Promise((resolve, reject) =>{
+
+    const parser = new XMLHttpRequest();
+    parser.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            resolve(JSON.parse(this.responseText));
+        }
+    };
+    parser.open("GET", "data.json", true);
+    parser.send();
+});
+
+
+promise.then(item => {
+    // We fetch the JSON content then continue to pipe it
+    // React like but hey, pure js right
+    let element = "";
+
+    item.forEach(data => {
+        let stack = [];
+        let change = document.createElement("div");
+        change.className = `block flex flex--a-center flex--j-between ${data.featured? 'featured' : ''}`
+        element = `
+            <div class="flex flex--a-center flex--name-gap">
+                <img class="website-image" src="${data.logo}" alt="logo of website">
+                <div class="flex flex--column flex--j-around flex--company-gap">
+                    <div class="flex flex--a-center flex--feature-gap">
+                        <p class="f-15 f-700 dd-cyan">${data.company}</p>
+                        ${data.new? '<div class="new dd-cyan-bg f-700">NEW!</div>' : ''}
+                        ${data.featured? '<div class="featured-true vdg-cyan-bg f-700">FEATURED</div>' : ''}
+                    </div>
+                    <p class="f-20 f-700">${data.position}</p>
+                    <div class="flex flex--contract-gap">
+                        <p class="n-links f-15 l-dark">${data.postedAt}</p>
+                        <p class="n-links f-15 l-dark">${data.contract}</p>
+                        <p class="n-links f-15 l-dark">${data.location}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex flex--skills-gap">
+                <a href="#" data-label="filterItem" class="f-14">${data.role}</a>
+                <a href="#" data-label="filterItem" class="f-14">${data.level}</a>
+                ${Array(data.languages.length)
+                .fill()
+                .map((item, index) =>{
+                    return `
+                    <a href="#" data-label="filterItem" class="f-14">${data.languages[index]}</a>
+                    `
+                })
+                .join("")}
+                ${Array(data.tools.length)
+                .fill()
+                .map((item, index) => {
+                    return `
+                    <a href="#" data-label="filterItem" class="f-14">${data.tools[index]}</a>
+                    `
+                })
+                .join("")}
+            </div>
+        `
+        stack.push(data.role, data.level, ...data.languages, ...data.tools);
+        change.innerHTML = element;
+        container.appendChild(change);
+        stackElements.push({element: change, filter: stack, available: true});
+    })
+    
+    return item;
+
+}).then(data =>{    
+    const filterItems = els("[data-label=filterItem]");
+
+    
+    filterItems.forEach(item =>{
+        item.addEventListener("click", function(event) {
+            event.preventDefault();
+            let a = this.cloneNode(true);
+            a.setAttribute("data-label", "filteredItem");
+
+            if(Object.keys(currentStack).length){ 
+                if(!(item.textContent in currentStack)){
+                    filterStack(item.textContent, a);
+                    filterEvent(a);
+                }
+            } else {
+                filterStack(item.textContent, a);
+                filterEvent(a);
+            }
+        })
+    })
+})
+
+// Needs to be prevented default
